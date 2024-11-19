b8b59a4c779d3b75a090ee54a8b2b2e900c05eb2
Venkatesan <15094840+morpheus-oss@users.noreply.github.com>
2024-11-19 15:54:08 +0530

    Update to pre-prod ready (Redis as DB, Container)
    Added Redis as DB and persist the short code and original URL.
    Dockerized the app.
    Added Docker-compose to orchestrate Redis and the App with the network.

--- shorten-url/src/main/java/dev/shorturl/controller/URLController.java
+++ shorten-url/src/main/java/dev/shorturl/controller/URLController.java
@@ -2,29 +2,23 @@ package dev.shorturl.controller;
 
 import dev.shorturl.service.UrlShortenerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class URLController {
 
     @Autowired
     private UrlShortenerService shortener;
 
-    @PostMapping("/generateURL")
-    public ResponseEntity<String> generateURL(@RequestBody String actualURL)   {
-        String shortURL = shortener.shortenURL(actualURL);
-        return new ResponseEntity<>(shortURL, HttpStatus.CREATED);
+    @GetMapping("/generate")
+    public String generateURL(@RequestParam("url") String actualURL)   {
+        return shortener.shortenURL(actualURL);
     }
 
-    @GetMapping("/redirect")
-    public ResponseEntity<Void> redirect(@PathVariable String shortURL)     {
-        String actualURL = shortener.getActualURL(shortURL);
-        if (actualURL == null)  {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return ResponseEntity.status(HttpStatus.FOUND).header("Location", actualURL).build();
+    @GetMapping("/{shortUrl}")
+    public String redirect(@PathVariable("shortUrl") String shortURL)     {
+        return shortener.getActualURL(shortURL);
     }
 
 }
--- shorten-url/src/main/java/dev/shorturl/service/UrlShortenerService.java
+++ shorten-url/src/main/java/dev/shorturl/service/UrlShortenerService.java
@@ -1,6 +1,7 @@
 package dev.shorturl.service;
 
-import dev.shorturl.Base62Encoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -8,18 +9,47 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class UrlShortenerService {
-    private ConcurrentHashMap<Long, String> urlMap = new ConcurrentHashMap<>();
-    private AtomicLong counter = new AtomicLong(1);
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+//    private final ConcurrentHashMap<String, String> urlMap = new ConcurrentHashMap<>();
+    private final AtomicLong counter = new AtomicLong();
 
     public String shortenURL(String actualURL)  {
         long id = counter.getAndIncrement();
-        String shortURL = Base62Encoder.encode(id);
-        urlMap.put(id, actualURL);
+        String shortURL = encode(id);
+        redisTemplate.opsForValue().set(shortURL, actualURL);
+//        urlMap.put(shortURL, actualURL);
         return shortURL;
     }
 
     public String getActualURL(String shortURL) {
-        long id = Base62Encoder.decode(shortURL);
-        return urlMap.get(id);
+        return redisTemplate.opsForValue().get(shortURL);
+//        return urlMap.get(shortURL);
+    }
+
+    private static final String BASE62_CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    private String encode(long num) {
+        StringBuilder encodedStr = new StringBuilder();
+        if (num == 0)   {
+            encodedStr.insert(0, BASE62_CHARACTERS.charAt(0));
+        }
+        while (num > 0) {
+            int remainder = (int) (num % 62);
+            encodedStr.insert(0, BASE62_CHARACTERS.charAt(remainder));
+            num /= 62;
+        }
+        return encodedStr.toString();
+    }
+
+    public static long decode(String base62Str) {
+        long num = 0;
+
+        for (int i = 0; i < base62Str.length(); i++) {
+            num = num * 62 + BASE62_CHARACTERS.indexOf(base62Str.charAt(i));
+        }
+
+        return num;
     }
 }
