6dad9d2499f685f8705723179679670519cc1692
Venkatesan <15094840+morpheus-oss@users.noreply.github.com>
2024-12-03 13:03:00 +0530

    Updated to fix the container names
    Fixed container name & updated for detailed response.

--- shorten-url/src/main/java/dev/shorturl/controller/URLController.java
+++ shorten-url/src/main/java/dev/shorturl/controller/URLController.java
@@ -2,7 +2,14 @@ package dev.shorturl.controller;
 
 import dev.shorturl.service.UrlShortenerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -11,14 +18,34 @@ public class URLController {
     @Autowired
     private UrlShortenerService shortener;
 
-    @GetMapping("/generate")
-    public String generateURL(@RequestParam("url") String actualURL)   {
-        return shortener.shortenURL(actualURL);
+    @PostMapping("/generate")
+    public ResponseEntity<Object> generateURL(@RequestParam("url") String actualURL)   {
+        if (!isValidURL(actualURL))   {
+            return new ResponseEntity<>("Invalid URL: " + actualURL, HttpStatus.BAD_REQUEST);
+        }
+        String shortURL = shortener.shortenURL(actualURL);
+        return new ResponseEntity<>(shortURL, HttpStatus.OK);
     }
 
     @GetMapping("/{shortUrl}")
-    public String redirect(@PathVariable("shortUrl") String shortURL)     {
-        return shortener.getActualURL(shortURL);
+    public ResponseEntity<Object> redirect(@PathVariable("shortUrl") String shortURL)     {
+        Optional<String> actualURL = Optional.ofNullable(shortener.getActualURL(shortURL));
+        ResponseEntity<Object> response = null;
+
+        response = actualURL.<ResponseEntity<Object>>map(s -> new ResponseEntity<>(s, HttpStatus.OK))
+                            .orElseGet(() -> new ResponseEntity<>(
+                "Provided short URL (" + shortURL + ") not found!", HttpStatus.NOT_FOUND));
+
+        return response;
+    }
+
+    private boolean isValidURL(String url)  {
+        try {
+            new URL(url).toURI();
+        } catch (MalformedURLException | URISyntaxException e) {
+            return false;
+        }
+        return true;
     }
 
 }
